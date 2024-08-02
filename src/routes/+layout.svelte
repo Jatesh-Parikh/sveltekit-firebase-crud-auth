ccd0bb0df1e3c331c2b1e56e8ae2065861409228
Jatesh Parikh <jateshparikh97@gmail.com>
2024-08-02 20:05:11 +0530

    initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<div class="mainContainer">
+	<slot />
+</div>
+
+<style>
+	.mainContainer {
+		min-height: 100vh;
+		background: linear-gradient(to right, #000428, #00004b, #050f64);
+		color: white;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
